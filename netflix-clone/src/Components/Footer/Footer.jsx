0cad08fb5da2502870138c1ca48079655e3311fe
Natnael <nattytibe@gmail.com>
2025-09-10 23:00:12 +0300

    Header and Footer

---
+++ netflix-clone/src/Components/Footer/Footer.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import styles from './footer.module.css'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
+
+const Footer = () => {
+  return (
+   <div className={styles.footer_outer_container}>
+    <div className= {styles.footer_inner_container}>
+      <div className= {styles.footer_icons}>
+<FacebookOutlinedIcon/>
+<InstagramIcon/>
+<YouTubeIcon/>
+      </div>
+      <div className={styles.footer_data}>
+        <div>
+          <ul>
+          <li>Audio Description</li>
+          <li>Investor Relations</li>
+          <li>Legal Notice</li>
+          </ul>
+        </div>
+        <div><ul>
+        <li>Help Center</li>
+        <li>Jobs</li>
+        <li>Cookie Preference</li>
+        </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Gift Cards</li>
+          <li>Terms of Users</li>
+          <li>Corporate Information</li>
+          </ul>
+        </div>
+        <ul>
+          <li>Media Center</li>
+          <li>Privacy</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+    </div>
+  <div className= {styles.service_code}>
+<p>Service Code</p>
+    </div>
+    <div className={styles.copy_write}>
+      &copy; 1997-2025 Netflix, Inc.
+    </div>
+   </div>
+    
+  )
+}
+
+export default Footer
