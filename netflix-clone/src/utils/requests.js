62840553c79739ada8fc4060284cc43ec33851a4
Natnael <nattytibe@gmail.com>
2025-09-11 23:50:25 +0300

    Axios-Request & env

---
+++ netflix-clone/src/utils/requests.js
@@ -0,0 +1,24 @@
+const API_KEY = process.env.REACT_APP_API_KEY ;
+const requests = {
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentary: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchTvShow:`/tv/popular?api_key=${API_KEY}api_key=${API_KEY}&language=en-US&page=1`
+
+}
+export default requests;
+
+
+
+
+
+
+
+
+
+// movie api= 0936c77ef6c75378b466d16865bc0557
